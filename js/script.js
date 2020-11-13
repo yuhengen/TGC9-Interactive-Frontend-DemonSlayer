dda7d96bae34802cf80c7e4cbd21a452ecf91305
Yu Heng <yuhengen@gmail.com>
2020-11-13 17:00:10 +0800

    Added a loading screen using css and js

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// Loading screen transition
+let overlay = document.querySelector("#overlay");
+
+// window.addEventListener("load", function () {
+//     overlay.style.opacity = "0"
+// });
+
+setTimeout(function() {
+    overlay.style.opacity = "0"
+},2000)
