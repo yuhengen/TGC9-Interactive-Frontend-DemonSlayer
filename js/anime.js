e0f06aa7aa70b15f94414c452fc2d4fb2fe491ac
yuhengen <yuhengen@gmail.com>
2020-11-15 06:17:56 +0000

    Styled anime.html

--- js/anime.js
+++ js/anime.js
@@ -13,10 +13,11 @@ async function displayInfo() {
     let animeRate = anime.imDbRating;
 
     let intro = document.querySelector("#anime-banner");
-    intro.innerHTML = `<div class="p-2"><img src="${animePoster}"/></div>
+    intro.innerHTML = `<div class="p-2"><img src="${animePoster}" height="400"/></div>
     <div class="p-2"><h1>${animeTitle}</h1><br/>
     <p>${animeDesc}</p><br/>
-    <p>IMDB Rating: ${animeRate}/10</p></div>`
+    <p>IMDB Rating: ${animeRate}/10</p><br/>
+    <p>Watch ${animeTitle} on: <a href="https://www.netflix.com/title/81091393">Netflix</a> | <a href="https://www.hulu.com/series/demon-slayer-kimetsu-no-yaiba-2c3e4b00-30d9-434d-bccc-cf346e40e868">Hulu</a></p></div>`
 }
 
 async function getEpisode() {
@@ -25,57 +26,57 @@ async function getEpisode() {
     return response.data;
 }
 
-// async function displayEpisode() {
-//     let epi = await getEpisode();
-
-//     for (let i = 0; i < epi.episodes.length; i++) {
-//         let epiTitle = epi.episodes[i].title;
-//         let epiImage = epi.episodes[i].image;
-//         let epiPlot = epi.episodes[i].plot;
-
+// function displayEpisode() {
+//     for (let i = 0; i < 25; i++) {
 //         let episodeInfo = document.querySelector("#episodes-info");
-//         episodeInfo.innerHTML = `<div id="heading${i}" class="card-header">
-//     <h3 class="mb-0">
-//         <button class="btn btn-link" data-toggle="collapse" data-target="#collapse${i}" aria-expanded="true" aria-controls="collapse${i}">
-//           <h3>Episode ${i} - ${epiTitle}</h3>
-//         </button>
-//       </h3>
-//     </div>
+//         episodeInfo.innerHTML += `<div id="heading${i}" class="card-header">
+//         <h3 class="mb-0">
+//             <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapse${i}" aria-expanded="false" aria-controls="collapse${i}">
+//               <h3>Episode ${i}<h3>
+//             </button>
+//           </h3>
+//         </div>
 
-//     <div id="collapse${i}" class="collapse show" aria-labelledby="heading${i}" data-parent="#accordion">
-//     <div class="d-flex align-items-center">
-//     <div><img src="${epiImage}"/></div>
-//       <div class="card-body">
-//         Episode Synopsis:<br/>
-//         <p>${epiPlot}</p>
-//       </div>
-//       </div>
-//     </div>`
+//         <div id="collapse${i}" class="collapse" aria-labelledby="heading${i}" data-parent="#accordion">
+//         <div class="d-flex align-items-center">
+//         <div>Image ${i}</div>
+//           <div class="card-body">
+//             Episode Synopsis:<br/>
+//             <p>Description ${i}</p>
+//           </div>
+//           </div>
+//         </div>`
 //     }
 // }
 
-function displayEpisode() {
-    for (let i = 0; i < 25; i++) {
+async function displayEpisode() {
+    let epi = await getEpisode();
+
+    for (let i = 0; i <= epi.episodes.length; i++) {
+        let epiTitle = epi.episodes[i].title;
+        let epiImage = epi.episodes[i].image;
+        let epiPlot = epi.episodes[i].plot;
+
         let episodeInfo = document.querySelector("#episodes-info");
         episodeInfo.innerHTML += `<div id="heading${i}" class="card-header">
-        <h3 class="mb-0">
-            <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapse${i}" aria-expanded="false" aria-controls="collapse${i}">
-              <h3>Episode ${i}<h3>
-            </button>
-          </h3>
-        </div>
+    <h3 class="mb-0">
+        <button class="btn btn-link" data-toggle="collapse" data-target="#collapse${i}" aria-expanded="false" aria-controls="collapse${i}">
+          <h3>Episode ${i+1} - ${epiTitle}</h3>
+        </button>
+      </h3>
+    </div>
 
-        <div id="collapse${i}" class="collapse" aria-labelledby="heading${i}" data-parent="#accordion">
-        <div class="d-flex align-items-center">
-        <div>Image ${i}</div>
-          <div class="card-body">
-            Episode Synopsis:<br/>
-            <p>Description ${i}</p>
-          </div>
-          </div>
-        </div>`
+    <div id="collapse${i}" class="collapse" aria-labelledby="heading${i}" data-parent="#accordion">
+    <div class="d-flex align-items-center">
+    <div><img src="${epiImage}" class="episode-image"/></div>
+      <div class="card-body">
+        <u>Episode Synopsis:</u><br/>
+        <p>${epiPlot}</p>
+      </div>
+      </div>
+    </div>`
     }
 }
 
+displayInfo();
 displayEpisode();
-// displayInfo();
