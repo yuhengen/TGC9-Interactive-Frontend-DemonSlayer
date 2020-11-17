cf0e1d950d2aaa2ea9e29eb715d28d4485269219
Yu Heng <yuhengen@gmail.com>
2020-11-17 13:10:14 +0800

    Added another character to the characters page. Added description below

--- js/characters.js
+++ js/characters.js
@@ -14,13 +14,5 @@ charIcon.addEventListener("click", async function (e) {
         document.querySelector("#character-lastname").innerHTML = character[e.target.id]["charLastName"];
         document.querySelector("#character-desc").innerHTML = `<p class="desc-text">${character[e.target.id]["charDescription"]}</p>`
         document.querySelector("#character-portrait").innerHTML = `<img src="${character[e.target.id]["charPortrait"]}" class="portrait-size"/>`
-    } 
-    // let charLastName = character.hasOwnProperty(e.target.id) ? character[e.target.id]["charLastName"]:"";
-    // let charDescription = character.hasOwnProperty(e.target.id) ? character[e.target.id]["charDescription"]:"";
-    // let charPortrait = character.hasOwnProperty(e.target.id) ? character[e.target.id]["charPortrait"]:"";
-
-    // // document.querySelector("#character-name").innerHTML = charName;
-    // document.querySelector("#character-lastname").innerHTML = charLastName;
-    // document.querySelector("#character-desc").innerHTML = `<p class="desc-text">${charDescription}</p>`;
-    // document.querySelector("#character-portrait").innerHTML = `<img src="${charPortrait}" class="portrait-size"/>`;
+    }
 })
