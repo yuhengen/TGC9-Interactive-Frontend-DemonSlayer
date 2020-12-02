0acfc1f1114ca04a6e095c1149104f872de27157
Yu Heng <yuhengen@gmail.com>
2020-12-02 16:14:10 +0800

    Removed background video for page smoothness. Added about.html page

---
+++ js/about.js
@@ -0,0 +1,14 @@
+// Loading screen transition
+let overlay = document.querySelector("#overlay");
+
+window.onload = function () {
+    setTimeout(function () {
+        let body = document.querySelector("body");
+        overlay.style.opacity = "0"
+        body.style.overflow = "scroll";
+    }, 2000)
+
+    setTimeout(function () {
+        overlay.remove()
+    }, 3000)
+};
