314c849cc231dc56ba3b23f69bf614abe6e85923
Yu Heng <yuhengen@gmail.com>
2020-12-02 16:26:13 +0800

    Removed glowing effect to reduce lag

--- js/characters.js
+++ js/characters.js
@@ -14,29 +14,28 @@ window.onload = function () {
 };
 
 async function getChar() {
-    let response = await axios.get("js/characters.json");
-    return response.data;
+  let response = await axios.get("js/characters.json");
+  return response.data;
 }
-
 
 let charIcon = document.querySelector(".character-menu")
 
 charIcon.addEventListener("click", async function (e) {
-    let character = await getChar();
+  let character = await getChar();
 
-    if (character.hasOwnProperty(e.target.id)) {
-        document.querySelector("#character-name").innerHTML = character[e.target.id]["charName"];
-        document.querySelector("#character-lastname").innerHTML = character[e.target.id]["charLastName"];
-        document.querySelector("#character-desc").innerHTML = `<p class="desc-text">${character[e.target.id]["charDescription"]}</p>`
-        document.querySelector("#character-portrait").innerHTML = `<img src="${character[e.target.id]["charPortrait"]}" class="portrait-size"/>`
-    }
+  if (character.hasOwnProperty(e.target.id)) {
+    document.querySelector("#character-name").innerHTML = character[e.target.id]["charName"];
+    document.querySelector("#character-lastname").innerHTML = character[e.target.id]["charLastName"];
+    document.querySelector("#character-desc").innerHTML = `<p class="desc-text">${character[e.target.id]["charDescription"]}</p>`
+    document.querySelector("#character-portrait").innerHTML = `<img src="${character[e.target.id]["charPortrait"]}" class="portrait-size"/>`
+  }
 })
 
 //Get the button
 let toTopBtn = document.querySelector(".toTopBtn");
 
 // When the user scrolls down 500px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function () { scrollFunction() };
 function scrollFunction() {
   if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
     toTopBtn.style.display = "block";
