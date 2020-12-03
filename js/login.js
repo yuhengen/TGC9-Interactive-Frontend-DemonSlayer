414398a111cb36ccc07ceb0eb2027dc5804cf2b5
yuhengen <yuhengen@gmail.com>
2020-12-03 18:13:48 +0000

    Added login window with Bootstrap modal

--- js/login.js
+++ js/login.js
@@ -1,26 +1,7 @@
-function loginmodal() {
-    let loginmodal = `<div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog"
-		aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-		<div class="modal-dialog modal-dialog-centered" role="document">
-			<div class="modal-content">
-				<div class="modal-header">
-					<h5 class="modal-title" id="exampleModalLongTitle">Modal title</h5>
-					<button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-				</div>
-				<div class="modal-body">
-					...
-				</div>
-				<div class="modal-footer">
-					<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-					<button type="button" class="btn btn-primary">Save changes</button>
-				</div>
-			</div>
-		</div>
-    </div>`
+// function loginmodal() {
+//     let loginmodal = ``
     
-    document.querySelector(".login-modal").innerHTML = loginmodal;
-}
+//     document.querySelector(".login-modal").innerHTML = loginmodal;
+// }
 
-loginmodal();
+// loginmodal();
